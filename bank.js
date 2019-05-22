32238864786edca238cfc832fc4d33b804fb5e12
katieburrows <katie_burrows@me.com>
2019-05-22 14:55:33 -0600

    total function working

--- bank.js
+++ bank.js
@@ -1,13 +1,29 @@
 var fs = require("fs");
 
 var operator = process.argv[2];
-
-
+var number = process.argv[3];
+var sum = 0;
 
 
 var total = function(){
+    fs.readFile("bank.txt", "utf8", function(error, data){
+        if (error) {
+            return console.log(error);
+        }
 
+        var dataArray = data.split(", ");
+        
+        for (var i = 0; i < dataArray.length; i++){
+            var num = parseFloat(dataArray[i]);
+            sum += num;
+            
+        }
+        return sum;
+       
+    });
 }
+
+
 
 var deposit = function(){
     
